e30b7a5c9d7314d82550d0ec634739554ff5fe81
shiv7727 <shiv7727@gmail.com>
2024-06-24 14:30:53 +0530

    posts data updte array dependency

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,9 +1,11 @@
+import React,{useState} from "react";
 import usePosts from './hooks/usePosts';
 
 
 
 const PostList = () => {
-  const {data,error,isLoading} = usePosts();
+  const [userId,setUserId] = useState<number>();
+  const {data,error,isLoading} = usePosts(userId);
 
 
   if(isLoading) return <p>Loading ...</p>;
@@ -11,13 +13,24 @@ const PostList = () => {
   if(error) return <p>{error.message}</p>;
 
   return (
-    <ul className="list-group">
-      {data?.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <select className="form-select mb-3"
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setUserId(parseInt(e.target.value))}
+        value={userId}
+      >
+        <option value=""></option>
+        <option value="1">user 1</option>
+        <option value="2">user 2</option>
+        <option value="3">user 3</option>
+      </select>
+      <ul className="list-group">
+        {data?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
