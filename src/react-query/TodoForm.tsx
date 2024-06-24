e353016556428c6be9da84fa41b1cb55ab728a2a
shiv7727 <shiv7727@gmail.com>
2024-06-24 22:59:49 +0530

    mutation error

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation({
+  const addTodo = useMutation<Todo,Error,Todo>({
     mutationFn: (todo: Todo) => axios.post<Todo>('https://jsonplaceholder.typicode.com/todos',todo).then(res => res.data),
     onSuccess: (savedTodo,newTodo) => {
       // APPROACH : 1 Invalidating the CACHE
@@ -38,14 +38,21 @@ const TodoForm = () => {
   };
 
   return (
-    <form className="row mb-3" onSubmit={handleSubmit} >
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+    <>
+      {addTodo.error && (
+        <div className="alert alert-danger">
+          {addTodo.error.message}
+        </div>
+      )}
+      <form className="row mb-3" onSubmit={handleSubmit} >
+        <div className="col">
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button className="btn btn-primary">Add</button>
+        </div>
+      </form>
+    </>
   );
 };
 
