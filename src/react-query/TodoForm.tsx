ad18a8746665c2cd2ed5930952be09c76daf442d
shiv7727 <shiv7727@gmail.com>
2024-06-25 16:15:50 +0530

    creating a reusable http service

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -34,8 +34,8 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary" disabled={addTodo.isLoading}>
-            {addTodo.isLoading ? "Adding..." : "Add"}
+          <button className="btn btn-primary" >
+            Add
           </button>
         </div>
       </form>
--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,27 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import { CACHE_KEY_TODOS } from '../constants';
+import todoService, { Todo } from '../services/todoService';
 
-export interface Todo {
-	id: number;
-	title: string;
-	userId: number;
-	completed: boolean;
-}
 const useTodos = () => {
-	const fetchTodos = () => {
-		return axios
-			.get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
-			.then((response) => response.data);
-	};
-
 	return useQuery<Todo[], Error>({
 		queryKey: CACHE_KEY_TODOS,
-		queryFn: fetchTodos,
-		// () =>
-		// 	axios
-		// 		.get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
-		// 		.then((response) => response.data),
+		queryFn: todoService.getAll,
 		staleTime: 10 * 1000,
 	});
 };
